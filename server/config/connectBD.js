cf13d914d6222d2c5996ab1e90e3ba31a4c2825e
Anhthu09 <Thuhapd10684@gmail.com>
2025-06-12 02:18:10 +0700

    ket noi du lieu

--- server/config/connectBD.js
+++ server/config/connectBD.js
@@ -3,6 +3,7 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Thu001122", // hoặc đúng mật khẩu MySQL của chị
+  password: "", // hoặc đúng mật khẩu MySQL của chị
   database: "datn2025",
+   port: 3308 
 });
--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import categoryRoutes from "./routes/categoryRoutes.js";
-
+import { db } from "./config/connectBD.js";
 const __dirname = import.meta.dirname;
 
 const app = express();
 
+// Test kết nối DB
+db.connect((err) => {
+  if (err) {
+    console.error("❌ Không thể kết nối MySQL:", err);
+  } else {
+    console.log("✅ Kết nối MySQL thành công!");
+  }
+});
 // app.use(express.static("public"));
 
 app.set("view engine", "ejs");
@@ -16,7 +24,6 @@ app.use("/api", categoryRoutes);
 // app.use("/api/products", productRouter);
 // app.use("/api", bookRoutes);
 // app.use("/api", reviewRoutes);
-
 
 app.listen(3000, () => {
   console.log("ExpressJS server started!!!");
